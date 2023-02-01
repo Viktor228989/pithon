cd6ba66f62d2b0d9cdddda60fe514108d42c43ea
Pasha3010754 <pavelemelianov763@gmail.com>
2023-02-01 21:00:08 +0700

    1 февраля

---
+++ lesson_18/main.py
@@ -0,0 +1,29 @@
+import easygui
+#
+# import easygui
+#
+# easygui.msgbox(
+#     msg="Hello world!",
+#     image="img/d.png"
+# )
+
+# easygui.buttonbox(
+#     msg="Куда сам сядешь...",
+#     choice=("Стул твердый" "Стул не твердый"),
+#     title="Интересная задача"
+# )
+
+# number = easygui.integerbox(
+#     msg="За сколько куищь слона",
+#     lowerbound=5,
+#     upperbound= 101,
+#     default= 25
+# )
+# print(number)
+
+# # text = easygui.enterbox(
+#     msg="Как твое имя, путник"
+#     title=""
+# )
+
+
